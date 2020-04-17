b8060e5786006f249e39d82e91569a94b21802cd
marcus-xjtu <marcus704@stu.xjtu.edu.cn>
2020-04-17 14:21:13 +0800

    环境配置更新+api+获取todolist测试

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  render,
+  fireEvent,
+  act,
+  wait,
+  getByTestId,
+  getAllByTestId,
+} from "@testing-library/react";
+import App from "./App";
+import * as TodoApi from "./api/TodoApi";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App>", () => {
+  const item = { id: 1, content: "First Item", createAt: "2020/04/16" };
+  const updateItem = { id: 1, content: "Update Item", createAt: "2020/04/16" };
+  const addedItem = { id: 2, content: "Second Item", createAt: "2020/04/16" };
+
+  beforeEach(() => {
+    jest
+      .spyOn(TodoApi, "getTodos")
+      .mockImplementation(() => Promise.resolve([item]));
+  });
+
+  test("should display todo items correctly", async () => {
+    await act(async () => {
+      render(<App />);
+    });
+
+    expect(getByTestId(document.body, "task-item")).toHaveTextContent(
+      "First Item"
+    );
+  });
+
 });
