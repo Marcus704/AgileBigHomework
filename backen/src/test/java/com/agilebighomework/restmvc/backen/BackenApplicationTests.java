95b57ffae37bd19d8d0c85bcc098f282b9c66278
NoahPlus <ljg247552025@qq.com>
2020-04-19 20:55:44 +0800

    获取测试+删除测试

--- backen/src/test/java/com/agilebighomework/restmvc/backen/BackenApplicationTests.java
+++ backen/src/test/java/com/agilebighomework/restmvc/backen/BackenApplicationTests.java
@@ -1,8 +1,13 @@
 package com.agilebighomework.restmvc.backen;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -17,6 +22,8 @@ import java.util.Date;
 class BackenApplicationTests {
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private TaskService taskService;
 
     @Test
     void addTask() {
@@ -25,5 +32,22 @@ class BackenApplicationTests {
         Assert.assertEquals(resp.getBody(), "success");
         Assert.assertEquals(resp.getStatusCode(), HttpStatus.OK);
     }
+
+    @Test
+    public void getAllTask() throws JsonProcessingException {
+        Task t = new Task();
+        ObjectMapper mapper = new ObjectMapper();
+        ResponseEntity<String> response = testRestTemplate.getForEntity("/api/tasks/", String.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assert.assertEquals(response.getBody(), mapper.writeValueAsString(taskService.getDb()));
+    }
+
+    @Test
+    public void deleteTaskByID() {
+        ResponseEntity<String> resp = testRestTemplate.exchange("/api/task/1", HttpMethod.DELETE, null, String.class);
+        Assert.assertEquals(resp.getStatusCode(), HttpStatus.OK);
+        Assert.assertEquals(resp.getBody(), "success");
+    }
+
 
 }
