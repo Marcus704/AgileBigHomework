27db25da3c3ffd74e055fa796fc7ea2787cc7eef
mike shao <643636057@qq.com>
2020-04-21 11:28:43 +0800

    测试修改

--- backen/src/test/java/com/agilebighomework/restmvc/backen/BackenApplicationTests.java
+++ backen/src/test/java/com/agilebighomework/restmvc/backen/BackenApplicationTests.java
@@ -1,13 +1,13 @@
 package com.agilebighomework.restmvc.backen;
 
+import com.agilebighomework.restmvc.backen.all.Task;
+import com.agilebighomework.restmvc.backen.all.TaskService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
@@ -15,22 +15,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.WebApplicationContext;
+
 import java.util.Date;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-class BackenApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BackenApplicationTests {
     @Autowired
     private TestRestTemplate testRestTemplate;
     @Autowired
     private TaskService taskService;
 
     @Test
-    void addTask() {
+    public void addTask() {
         Task t=new Task("3", "test", new Date());
         ResponseEntity<String> resp = testRestTemplate.postForEntity("/api/task", t, String.class);
         Assert.assertEquals(resp.getBody(), "success");
